1a1b8ffd55657b08990af96eea533f3dfb069952
havegood95@gmail.com <qwert12345>
2018-11-25 15:50:25 +0300

    optimazed singleThreadMultiply method and added concurrentMultiply realisation

--- src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -1,8 +1,10 @@
 package ru.javaops.masterjava.matrix;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
+import java.util.concurrent.*;
 
 /**
  * gkislin
@@ -10,24 +12,65 @@ import java.util.concurrent.ExecutorService;
  */
 public class MatrixUtil {
 
-    // TODO implement parallel multiplication matrixA*matrixB
     public static int[][] concurrentMultiply(int[][] matrixA, int[][] matrixB, ExecutorService executor) throws InterruptedException, ExecutionException {
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
 
+        int BT[][] = new int[matrixSize][matrixSize];
+        for (int i = 0; i < matrixSize; i++) {
+            for (int j = 0; j < matrixSize; j++) {
+                BT[j][i] = matrixB[i][j];
+            }
+        }
+
+        final CompletionService<LineResult> completionService = new ExecutorCompletionService<>(executor);
+        List<Future> futures = new ArrayList<>();
+        for (int i = 0; i < matrixSize; i++) {
+            int finalI = i;
+            futures.add(completionService.submit(() -> calculateLine(finalI, matrixA, BT)));
+        }
+
+        while (!futures.isEmpty()) {
+            Future<LineResult> future = completionService.poll(10, TimeUnit.SECONDS);
+            if (future == null) {
+                throw new InterruptedException();
+            }
+            futures.remove(future);
+            LineResult lineResult = future.get();
+            System.arraycopy(lineResult.value, 0, matrixC[lineResult.line], 0, matrixSize);
+        }
         return matrixC;
     }
 
-    // TODO optimize by https://habrahabr.ru/post/114797/
+
+    private static LineResult calculateLine(int lineIndex, int[][] matrixA, int[][] BT) {
+        final int matrixSize = matrixA.length;
+        final int[] lineResults = new int[matrixSize];
+        for (int j = 0; j < matrixSize; j++) {
+            int sum = 0;
+            for (int k = 0; k < matrixSize; k++) {
+                sum += matrixA[lineIndex][k] * BT[j][k];
+            }
+            lineResults[j] = sum;
+        }
+        return new LineResult(lineIndex, lineResults);
+    }
+
     public static int[][] singleThreadMultiply(int[][] matrixA, int[][] matrixB) {
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
 
+        int BT[][] = new int[matrixSize][matrixSize];
+        for (int i = 0; i < matrixSize; i++) {
+            for (int j = 0; j < matrixSize; j++) {
+                BT[j][i] = matrixB[i][j];
+            }
+        }
         for (int i = 0; i < matrixSize; i++) {
             for (int j = 0; j < matrixSize; j++) {
                 int sum = 0;
                 for (int k = 0; k < matrixSize; k++) {
-                    sum += matrixA[i][k] * matrixB[k][j];
+                    sum += matrixA[i][k] * BT[j][k];
                 }
                 matrixC[i][j] = sum;
             }
@@ -57,5 +100,24 @@ public class MatrixUtil {
             }
         }
         return true;
+    }
+
+    private static class LineResult {
+
+        private int line;
+        private int value[];
+
+        public LineResult(int i, int value[]) {
+            this.line = i;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "LineResult{" +
+                    "line=" + line +
+                    ", value=" + Arrays.toString(value) +
+                    '}';
+        }
     }
 }
